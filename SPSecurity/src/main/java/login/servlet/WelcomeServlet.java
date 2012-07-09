121d16451b530169430467b0951e5ffca5d68b9a
olive.naveen.santo <olive.naveen.santo@gmail.com>
2012-07-09 17:03:41 +0530

    second commit

---
+++ SPSecurity/src/main/java/login/servlet/WelcomeServlet.java
@@ -0,0 +1,39 @@
+package login.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    /**
+     * Default constructor. 
+     */
+    public WelcomeServlet() {
+        // TODO Auto-generated constructor stub
+    	System.out.println(this.getServletName() + "Got Initialized");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getOutputStream().println("From your GET Method");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getOutputStream().println("From your POST Method");
+	}
+}
